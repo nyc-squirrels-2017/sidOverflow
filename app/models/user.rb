67a7013ad7f22ddc27f9ba5893790ad099f9e57a
Kendy Ng & Sid Ahmed Merzouk <pair+kenobi82403.sid-ah@devbootcamp.com>
2017-02-13 10:24:36 -0500

    create users migration and model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_secure_password
+  has_many :questions, dependent: :destroy
+
+  validates :username, :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 2 }
+
+end
