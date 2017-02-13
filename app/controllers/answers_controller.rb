8c2706ba27ad58f15492fd75d020ea19a92ae423
Kendy Ng & Sid Ahmed Merzouk <pair+kenobi82403.sid-ah@devbootcamp.com>
2017-02-13 16:20:32 -0500

    create update and destroy routes

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -11,6 +11,31 @@ class AnswersController < ApplicationController
     end
   end
 
+  def edit
+    @answer = Answer.find_by_id(params[:id])
+  end
+
+  def update
+    @answer = Answer.find_by_id(params[:id])
+
+    if authorized?(@answer.user_id) && @answer.update(answer_params)
+      @question = Question.find_by_id(@answer.question_id)
+      redirect_to @question
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @answer = Answer.find_by_id(params[:id])
+    @question = Question.find_by_id(@answer.question_id)
+
+    if authorized?(@answer.user_id)
+      @answer.destroy
+      redirect_to @question
+    end
+  end
+
   private
 
   def answer_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :questions do
-    resources :answers, except: [:index, :show]
+    resources :answers, only: [:create]
   end
+  resources :answers, only: [:edit, :update, :destroy]
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :destroy]
 
