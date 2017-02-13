b920d3441a9420733d6d360263cfac0ff370388a
Kendy Ng & Sid Ahmed Merzouk <pair+kenobi82403.sid-ah@devbootcamp.com>
2017-02-13 10:52:50 -0500

    create new question form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,9 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
 
+  def new
+    @question = Question.new
+  end
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213150143) do
+ActiveRecord::Schema.define(version: 20170213155030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170213150143) do
     t.string   "question",   null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
     t.index ["user_id"], name: "index_questions_on_user_id", using: :btree
   end
 
